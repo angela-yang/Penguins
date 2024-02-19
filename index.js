99bc4d744af41e248c97ce55dd8237e8715301a2
Gjones747 <gavinjones-personal@Gavins-MacBook-Air.local>
2024-02-19 14:04:15 -0800

    mombo

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 
-app.use(express.static(path.join(__dirname, '/static')))
+app.use(express.static(path.join(__dirname, '.templates/assets')))
 
 app.get('/', (req, res) => {
     console.log('here')
