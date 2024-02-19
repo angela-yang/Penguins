7118f35ac49e871e99f58258038cae22520cc310
Gavin Jones - Personal <gavinjones-personal@Gavins-MacBook-Air.local>
2024-02-19 12:25:37 -0800

    start website

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require('path')
+
+const app = express()
+const port = 8056;
+
+
+app.use(express.json())
+
+
+
+app.use(express.static(path.join(__dirname, '/static')))
+
+app.get('/', (req, res) => {
+    console.log('here')
+    res.sendFile(path.join(__dirname,'./templates/app.html'))
+})
+
+app.listen(port, () => {
+    console.log("running on port 8056 localhost:8056")
+})
