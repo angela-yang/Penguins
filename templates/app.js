a46af8037f67ad8675ad83c297baaea53b9c84b7
Angela Yang <angela@MacBook-Air-2.local>
2024-02-19 17:07:18 -0800

    adopt

--- templates/app.js
+++ templates/app.js
@@ -1,2 +1,11 @@
+// p = document.getElementById("polarchoice");
 
-
+// function setVariables() {
+//     if (p=1) {
+//         document.getElementById('img').style = url("PolarBear1.png");
+//     } else if (p=2) {
+//         document.getElementById('img') = url("PolarBear2.png");
+//     } else {
+//         document.getElementById('img').style.background = url("PolarBear3.png");
+//     }
+// }
